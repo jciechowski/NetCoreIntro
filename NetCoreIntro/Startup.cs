c4e47207edaa60e3177a98b17767d0c0125b51fe
Jakub Ciechowski <jciechowski@gmail.com>
2019-03-09 11:26:25 +0100

    custom response for health check straight from docs

--- NetCoreIntro/Startup.cs
+++ NetCoreIntro/Startup.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace NetCoreIntro
 {
@@ -28,11 +34,12 @@ namespace NetCoreIntro
             services.AddHealthChecks().AddCheck<DbHealthCheck>(connectionString);
 
             #endregion
+
             #region Out of the box DB health check
 
-//            services
-//                .AddHealthChecks()
-//                .AddDbContextCheck<CoffeeDBContext>();
+            services
+                .AddHealthChecks()
+                .AddDbContextCheck<CoffeeDBContext>();
 
             #endregion
 
@@ -44,7 +51,10 @@ namespace NetCoreIntro
 
         public void Configure(IApplicationBuilder app)
         {
-            app.UseHealthChecks("/health");
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = WriteResponse
+            });
 
             #region Middleware
 
@@ -59,6 +69,24 @@ namespace NetCoreIntro
             app.UseMvc();
 
             #endregion
+        }
+
+        private static Task WriteResponse(HttpContext httpContext,
+            HealthReport result)
+        {
+            httpContext.Response.ContentType = "application/json";
+
+            var json = new JObject(
+                new JProperty("status", result.Status.ToString()),
+                new JProperty("results", new JObject(result.Entries.Select(pair =>
+                    new JProperty(pair.Key, new JObject(
+                        new JProperty("status", pair.Value.Status.ToString()),
+                        new JProperty("description", pair.Value.Description),
+                        new JProperty("data", new JObject(pair.Value.Data.Select(
+                            p => new JProperty(p.Key, p.Value))))))))));
+
+            return httpContext.Response.WriteAsync(
+                json.ToString(Formatting.Indented));
         }
     }
 }
